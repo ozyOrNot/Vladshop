498d31583610b079be4622098a7aaa56e4d233ec
Vlad Rah <rahmadjanov.of@gmail.com>
2023-04-01 23:58:09 +0300

    commit

--- base/views.py
+++ base/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from .models import *
 
-def BaseView():
-
-    context={}
-    
-    return render(request, '', context)
-
+def mainPage(request):
+    products = SmartPhone.objects.all()
+    notebooks = Notebook.objects.all()
+    context = {'products':products, 'notebooks':notebooks}
+    return render(request, 'main.html', context)
 
 
 # Create your views here.
